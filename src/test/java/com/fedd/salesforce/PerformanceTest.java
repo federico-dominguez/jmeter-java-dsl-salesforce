8408e865fd6e7d93d9423e207f78ae2bad7d200d
Federico Dominguez <sdominguez.federico@gmail.com>
2025-11-21 22:35:27 -0300

    fix: use repo-relative CSV path and clean/upload generated JTLs in CI

--- src/test/java/com/fedd/salesforce/PerformanceTest.java
+++ src/test/java/com/fedd/salesforce/PerformanceTest.java
@@ -63,7 +63,7 @@ public class PerformanceTest {
                                                 vars().set("BASE_URL",
                                                                 "orgfarm-b8d4a27e18-dev-ed.develop.my.salesforce.com"),
                                                 csvDataSet(
-                                                                "C:/Users/Fede/OneDrive/Documents/jmeter-java-dsl-salesforce/src/main/resources/data/leads_data.csv")
+                                                                "/src/main/resources/data/leads_data.csv")
                                                                 .ignoreFirstLine()
                                                                 .variableNames("p_lastname", "p_company",
                                                                                 "p_email_prefix", "p_leadsource",
