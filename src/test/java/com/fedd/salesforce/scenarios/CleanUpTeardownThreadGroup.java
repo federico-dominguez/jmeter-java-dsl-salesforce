dba5b135fcdeda0dcb9df36ab0f11c4b5004e2c9
Federico Dominguez <sdominguez.federico@gmail.com>
2025-11-28 11:44:20 -0300

    feat: implemented case service

--- src/test/java/com/fedd/salesforce/scenarios/CleanUpTeardownThreadGroup.java
+++ src/test/java/com/fedd/salesforce/scenarios/CleanUpTeardownThreadGroup.java
@@ -3,9 +3,8 @@ package com.fedd.salesforce.scenarios;
 import static us.abstracta.jmeter.javadsl.JmeterDsl.httpHeaders;
 import static us.abstracta.jmeter.javadsl.JmeterDsl.teardownThreadGroup;
 
-import org.checkerframework.checker.units.qual.t;
-
 import com.fedd.salesforce.services.AccountService;
+import com.fedd.salesforce.services.CaseService;
 import com.fedd.salesforce.services.LeadService;
 import com.fedd.salesforce.services.NoteService;
 import com.fedd.salesforce.services.OpportunityService;
@@ -20,6 +19,7 @@ public class CleanUpTeardownThreadGroup {
         private final OpportunityService opportunityService = new OpportunityService();
         private final NoteService noteService = new NoteService();
         private final TaskService taskService = new TaskService();
+        private final CaseService caseService = new CaseService();
 
         public DslTeardownThreadGroup getTeardownThreadGroup() {
                 return teardownThreadGroup("Clean up")
@@ -31,7 +31,8 @@ public class CleanUpTeardownThreadGroup {
                                                 noteService.deleteAllNotes().generateParentSample(),
                                                 opportunityService.deleteAllOpportunities().generateParentSample(),
                                                 accountService.deleteAllAccounts().generateParentSample(),
-                                                leadService.deleteAllLeads().generateParentSample());
+                                                leadService.deleteAllLeads().generateParentSample(),
+                                                caseService.deleteAllCases().generateParentSample());
         }
 
 }
--- src/test/java/com/fedd/salesforce/scenarios/TestServiceThreadGroup.java
+++ src/test/java/com/fedd/salesforce/scenarios/TestServiceThreadGroup.java
@@ -4,15 +4,15 @@ import static us.abstracta.jmeter.javadsl.JmeterDsl.httpHeaders;
 import static us.abstracta.jmeter.javadsl.JmeterDsl.threadGroup;
 import static us.abstracta.jmeter.javadsl.JmeterDsl.transaction;
 
+import com.fedd.salesforce.services.CaseService;
 import com.fedd.salesforce.services.LeadService;
-import com.fedd.salesforce.services.TaskService;
 
 import us.abstracta.jmeter.javadsl.core.threadgroups.DslDefaultThreadGroup;
 
 public class TestServiceThreadGroup {
 
     private final LeadService leadService = new LeadService();
-    private final TaskService taskService = new TaskService();
+    private final CaseService caseService = new CaseService();
 
     public DslDefaultThreadGroup getServiceThreadGroup(int users, int iterations) {
         return threadGroup("Lead to Cash", users, iterations,
@@ -21,7 +21,8 @@ public class TestServiceThreadGroup {
                 transaction("Test Services Process")
                         .generateParentSample(),
                 leadService.createLead(),
-                taskService.createTask());
+                caseService.createCase()
+                );
     }
 
 }
