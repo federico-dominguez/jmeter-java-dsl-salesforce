5989dee16ec1f9455fbb675b5829af29ee834814
Federico Dominguez <sdominguez.federico@gmail.com>
2025-11-28 13:34:34 -0300

    fix: removed testService from performance test

--- src/test/java/com/fedd/salesforce/tests/TestServiceTestPlan.java
+++ src/test/java/com/fedd/salesforce/tests/TestServiceTestPlan.java
@@ -16,31 +16,30 @@ import us.abstracta.jmeter.javadsl.core.DslTestPlan;
 
 public class TestServiceTestPlan {
 
-    private final AuthenticationSetupThreadGroup authGroup = new AuthenticationSetupThreadGroup();
-    private final CleanUpTeardownThreadGroup cleanUpGroup = new CleanUpTeardownThreadGroup();
-    private final TestServiceThreadGroup testServiceGroup = new TestServiceThreadGroup();
+        private final AuthenticationSetupThreadGroup authGroup = new AuthenticationSetupThreadGroup();
+        private final CleanUpTeardownThreadGroup cleanUpGroup = new CleanUpTeardownThreadGroup();
+        private final TestServiceThreadGroup testServiceGroup = new TestServiceThreadGroup();
 
-    public DslTestPlan getTestPlan() throws IOException {
-        return testPlan()
-                .children(
-                        vars().set("BASE_URL",
-                                "orgfarm-b8d4a27e18-dev-ed.develop.my.salesforce.com")
-                                .set("ownerId", "005gK00000AQ0ppQAD"),
-                        csvDataSet(
-                                "src/main/resources/data/leads_data.csv")
-                                .ignoreFirstLine()
-                                .variableNames("p_lastname", "p_company",
-                                        "p_email_prefix", "p_leadsource",
-                                        "p_amount"),
-                        httpCache()
-                                .disable(),
-                        httpCookies()
-                                .disable(),
-                        authGroup.getSetupThreadGroup(),
-                        testServiceGroup.getServiceThreadGroup(1, 1)
-                        //,cleanUpGroup.getTeardownThreadGroup()
-                );
+        public DslTestPlan getTestPlan() throws IOException {
+                return testPlan()
+                                .children(
+                                                vars().set("BASE_URL",
+                                                                "orgfarm-b8d4a27e18-dev-ed.develop.my.salesforce.com")
+                                                                .set("ownerId", "005gK00000AQ0ppQAD"),
+                                                csvDataSet(
+                                                                "src/main/resources/data/leads_data.csv")
+                                                                .ignoreFirstLine()
+                                                                .variableNames("p_lastname", "p_company",
+                                                                                "p_email_prefix", "p_leadsource",
+                                                                                "p_amount"),
+                                                httpCache()
+                                                                .disable(),
+                                                httpCookies()
+                                                                .disable(),
+                                                authGroup.getSetupThreadGroup(),
+                                                testServiceGroup.getServiceThreadGroup(1, 1),
+                                                cleanUpGroup.getTeardownThreadGroup());
 
-    }
+        }
 
 }
